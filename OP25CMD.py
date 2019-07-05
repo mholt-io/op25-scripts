64595edf7961cecd3a8985b46b1d4f10e4de5164
Michael Holt <michael.holt@ladb.com>
2019-07-05 13:35:37 -0700

    change everything to lowercase

--- OP25CMD.py
+++ OP25CMD.py
@@ -2,146 +2,146 @@ import os
 class Burbank():
     def Verdugo_All():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_All.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_all.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def BPD():
+    def bpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/BPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/bpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def GPD():
+    def gpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/GPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/gpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red1():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red1.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdgo_r.*.rsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red2():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red3():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red4():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red5():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red6():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red7():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red8():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red9():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Burbank/Verdugo_Red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/burbank/verdugo_red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
 class Glendale():
     def Verdugo_All():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_All.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/Verdugo_All.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def BPD():
+    def bpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/BPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/bpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def GPD():
+    def gpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/GPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/gpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red1():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red1.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red1.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red2():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red3():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red4():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red5():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red6():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red7():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red8():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red9():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Glendale/Verdugo_Red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/glendale/verdugo_red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
 class Pasadena():
     def Verdugo_All():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_All.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/Verdugo_All.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def BPD():
+    def bpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/BPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/bpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
-    def GPD():
+    def gpd():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/GPD.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/gpd.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red1():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red1.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red1.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red2():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red2.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red3():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red3.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red4():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red4.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red5():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red5.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red6():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red6.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red7():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red7.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red8():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red8.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
 
     def Verdugo_Red9():
         os.system("pkill -f ./rx.py")
-        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/TSV_Files/Pasadena/Verdugo_Red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
+        os.system("cd /home/pi/op25/op25/gr-op25_repeater/apps; ./rx.py --args 'rtl' -N 'LNA:49' -S 2400000 -o 25000 -q -1 -T /home/pi/op25-scripts/tsv_files/pasadena/verdugo_red9.tsv -V -2 -U 2> stderr.2 -l 'http:0.0.0.0:8080'&")
